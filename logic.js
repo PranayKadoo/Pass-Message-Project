8ee27fcdccfdc773ffec8b28fe2ab308e38e2443
PranayKadoo <pranaykadoo@gmail.com>
2021-08-27 20:15:28 +0530

    first push

---
+++ logic.js
@@ -0,0 +1,9 @@
+
+function passing(){
+     let text = document.getElementById("text").value;
+      let fetch = text;
+   document.getElementById("fetch").textContent = fetch;
+//  console.log(fetch);
+ console.log(fetch);
+
+}
